926985975f6c419e02c23530af4de24161f01d17
adrianglezlop <entornoc9@gmail.com>
2021-11-28 16:08:50 +0000

    vale1

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ Rails.application.routes.draw do
   get 'credits/:id/editpdf', to:'credits#editpdf',as:'editpdf'
   get 'view_credits/:id/pagos', to:'view_credits#pagos',as:'creditpagos'
   resources :credits
-  get 'credits/nuevo', to:'credits#formvale'
+  get 'credits/nuevo'
   
   resources :payouts
   resources :loans
