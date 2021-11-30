0f4dce46dc5cead11561b79c275a18fa360938ff
adrianglezlop <entornoc9@gmail.com>
2021-11-30 23:25:15 +0000

    vale2

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -136,7 +136,6 @@ class CreditsController < ApplicationController
     end
     def create_customer
       customer = @credit.customer
-       customer=Customer.find_by(CURP:@credit.CURP) if customer.nil?
         if customer.nil?
           customer=Customer.create(credit_params.except(:pdf64,:destination_id,:casa_color,:cancel_o_porton,:color_de_cancel,:caracteristicas_especiales,:antiguedad_laboral_meses,:antiguedad_laboral_anos,:product_id,:antiguedad_en_el_domicilio_anterior_anos,:antiguedad_en_el_domicilio_anterior_meses,:antiguedad_en_el_domicilio_actual_meses,:antiguedad_en_el_domicilio_actual_anos,:fecha,:es_cliente,:monto_solicitud,:cada_cuanto_se_realizara_el_pago,:lugar_donde_se_realizara_el_pago))
         else
