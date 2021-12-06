18e2216f4e4937c8657e832572d9e4840895188f
adrianglezlop <entornoc9@gmail.com>
2021-12-06 16:24:12 +0000

    vale7

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -62,7 +62,7 @@ class CreditsController < ApplicationController
   # GET /credits/1/edit
   def edit
     if @credit.product_id == 10
-      
+      format.html { redirect_to @credit, notice: 'Credit was successfully created.' }
     else
         
     end
