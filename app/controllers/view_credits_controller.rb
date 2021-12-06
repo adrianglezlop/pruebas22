8baf2bb5e9027644231f7d88e213abb5385b6b89
adrianglezlop <entornoc9@gmail.com>
2021-12-06 18:49:35 +0000

    vale7

--- app/controllers/view_credits_controller.rb
+++ app/controllers/view_credits_controller.rb
@@ -80,8 +80,8 @@ class ViewCreditsController < ApplicationController
     end
       
     if @credit.product_id == 10
+      pdf = ContratoPdf.new(@credit)
       
-      send_data pdf.render, filename: 'report.pdf', type: 'application/pdf', disposition: "inline"
     else
       pdf = ContratoPdf.new(@credit)
       send_data pdf.render, filename: 'report.pdf', type: 'application/pdf', disposition: "inline"
