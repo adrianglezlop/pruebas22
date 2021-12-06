dfa5054f430215e2edf90669e7e6004620b26ccb
adrianglezlop <entornoc9@gmail.com>
2021-12-06 15:37:55 +0000

    vale7

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -82,7 +82,7 @@ class CreditsController < ApplicationController
     respond_to do |format|
       if @credit.save
         if @credit.product_id == 10
-          format.html { redirect_to "/view_credits/index", notice: 'Credit was successfully updated.' }
+          format.html { redirect_to "/view_credits/index", notice: 'Credit was successfully updatedd.' }
         else
           format.html { redirect_to @credit, notice: 'Credit was successfully created.' }
           format.json { render :show, status: :created, location: @credit }  
