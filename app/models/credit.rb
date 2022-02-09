d7b7f6d26319f433a1588cc00a262f3862ed2427
adrianglezlop <entornoc9@gmail.com>
2022-02-09 19:57:40 +0000

    limite_cred

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -401,7 +401,7 @@ class Credit < ActiveRecord::Base
     end
     
     def limite_cred
-        a=@credit.referencia_agente_empresa
+        a=self.referencia_agente_empresa
         limcred = a.credits.where("self.status = ? and self.vale = ?", 1 ,1).sum("self.monto_solicitud").to_f
     end
 end
