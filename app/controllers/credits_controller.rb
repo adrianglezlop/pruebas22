0939e891c2be5abe0051ea3ee23e36f667806c40
adrianglezlop <entornoc9@gmail.com>
2022-03-15 22:31:42 +0000

    credvale

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,7 +1,5 @@
 class CreditsController < ApplicationController
-  if @credit.vale = 1
     layout "layouts/credvale"
-  end
 
   before_action :set_credit, only: [:show, :edit, :editpdf, :update, :destroy,:aprobar,:pdf,:text,:cancelar]
 
