f6dba40e864d46baf54dbf5d018b5f69a7930b99
adrianglezlop <entornoc9@gmail.com>
2022-04-06 15:52:52 +0000

    dtkta cuentas2

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,8 @@ Rails.application.routes.draw do
   get 'reports/usuarios'
   get 'reports/agentes'
   get 'reports/dtkta_clientes'
-   get 'reports/dtkta_cuentas'
+  get 'reports/dtkta_cuentas'
+  get 'reports/dtkta_perfil'
   
   get 'multiprint',to:"tickets#multiprint" ,as: "multiprint"
   put 'cancelar/:id', to:'tickets#cancelar', as: "cancelar"
