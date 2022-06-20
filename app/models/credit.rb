3ad1a4e66ad926841be5cee579d5348257885dc8
adrianglezlop <entornoc9@gmail.com>
2022-06-20 16:01:29 +0000

    monto 5000

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -106,7 +106,9 @@ class Credit < ActiveRecord::Base
     
      validates :referencia_agente_empresa, numericality: { other_than: 0 }
      
-     validates :monto_solicitud, numericality: { less_than: 3001 }, :if => 'product_id == 10' 
+     validates :monto_solicitud, numericality: { less_than: 3001 }, :if => 'product_id == 10'
+     validates :monto_solicitud, numericality: { less_than: 5001 }, :if => 'product_id == 11' 
+     validates :monto_solicitud, numericality: { less_than: 5001 }, :if => 'product_id == 12' 
      validates :agente_empresa,
     :inclusion => { :in => [nil,1, 0] }
     # 0 company 
